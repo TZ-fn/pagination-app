ff93ef2a0fa98f15737c362a069f7aa2224fb348
Tomek <tomekz@vp.pl>
2024-03-16 22:12:53 +0100

    feat: refactor the query function

--- src/queryFunctions/queryData.ts
+++ src/queryFunctions/queryData.ts
@@ -1,7 +1,15 @@
 const BASIC_API = "https://reqres.in/api/products";
 
-export default async function queryData() {
-  const response = await fetch(BASIC_API);
-  const data = await response.json();
-  return data;
+export default async function queryData(page: number = 1) {
+  if (page === 1) {
+    const response = await fetch(`${BASIC_API}?page=1`);
+    const data = await response.json();
+    return { page1: data, page2: { data: [] } };
+  } else {
+    const response1 = await fetch(`${BASIC_API}?page=${page - 1}`);
+    const response2 = await fetch(`${BASIC_API}?page=${page}`);
+    const data1 = await response1.json();
+    const data2 = await response2.json();
+    return { page1: data1, page2: data2 };
+  }
 }
