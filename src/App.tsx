0b3269671788f1f4d02fabf49684a53d96985f62
Tomek <tomekz@vp.pl>
2024-03-16 22:14:12 +0100

    feat: add fetching and paginating logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-import { useQuery } from "@tanstack/react-query";
-import queryLatestData from "./queryFunctions/queryData";
+import { useEffect, useState } from "react";
+import { keepPreviousData, useQuery } from "@tanstack/react-query";
+import queryData from "./queryFunctions/queryData";
 import Button from "./components/Button/Button";
 import TableRow from "./components/Table/TableRow/TableRow";
 import Table from "./components/Table/Table";
@@ -9,16 +10,50 @@ import IconRight from "./components/icons/IconArrowRight";
 import "./App.css";
 
 function App() {
-  const { isPending, error, data } = useQuery({
-    queryKey: ["items"],
-    queryFn: queryLatestData,
-  });
+  // set the default page number when the app loads and there is no page already set
+  const url = new URL(location.toString());
+  if (!url.searchParams.has("page")) {
+    url.searchParams.set("page", "1");
+    history.pushState({}, "", url);
+  }
 
-  const params = new URLSearchParams(location.search);
+  const pageNumber = Number(new URLSearchParams(location.search).get("page")) ?? 1;
+
+  const [currentPage, setCurrentPage] = useState(pageNumber);
+
+  const viewItemIndexes = {
+    1: [0, 1, 2, 3, 4],
+    2: [5, 6, 7, 8, 9],
+    3: [4, 5],
+  };
+
+  function handlePageChange(direction: "backwards" | "forwards") {
+    const url = new URL(location.toString());
+    const directionModifier = direction === "backwards" ? -1 : 1;
+    setCurrentPage((pageNumber) => pageNumber + directionModifier);
+    url.searchParams.set("page", `${pageNumber + directionModifier}`);
+    history.pushState({}, "", url);
+  }
+
+  const { isPending, error, data } = useQuery({
+    queryKey: ["items", currentPage],
+    queryFn: () => queryData(pageNumber),
+    placeholderData: keepPreviousData,
+  });
 
   if (isPending) return "Loading...";
 
   if (error) return `There was an error: ${error.message}`;
+
+  const numberOfPages = Math.ceil(data.page1.total % 5);
+
+  const results: ItemType[] = [];
+
+  if (data.page1.data && data.page2.data) {
+    viewItemIndexes[currentPage].map((index: number) => {
+      return results.push([...data.page1.data, ...data.page2.data][index]);
+    });
+  }
 
   return (
     <div className="max-w-2xl px-6 mx-auto flex flex-col align-middle justify-center">
@@ -31,17 +66,23 @@ function App() {
           placeholder="Enter the ID you're looking for..."
         />
       </label>
+      {console.log(results)}
       <Table>
-        {data.data.map(({ id, name, year, color }: ItemType) => (
-          <TableRow key={id} id={id} name={name} year={year} color={color} />
-        ))}
+        {results &&
+          results.length > 1 &&
+          results.map(({ id, name, year, color }: ItemType) => {
+            return <TableRow key={id} id={id} name={name} year={year} color={color} />;
+          })}
       </Table>
       <div className="flex align-middle justify-center gap-2 my-4">
-        <Button isDisabled={params.get("page") === null}>
+        <Button onClick={() => handlePageChange("backwards")} isDisabled={currentPage === 1}>
           <span className="sr-only">Previous 5 items</span>
           <IconArrowLeft />
         </Button>
-        <Button>
+        <Button
+          onClick={() => handlePageChange("forwards")}
+          isDisabled={currentPage !== null && currentPage > numberOfPages}
+        >
           <span className="sr-only">Next 5 items</span>
           <IconRight />
         </Button>
