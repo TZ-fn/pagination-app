a28992a3d324971ed3ae7a2905f06d7280e38feb
Tomek <tomekz@vp.pl>
2024-03-17 13:44:52 +0100

    fix: add error handling to the query functions

--- src/queryFunctions/queryData.ts
+++ src/queryFunctions/queryData.ts
@@ -3,11 +3,18 @@ const BASIC_API = "https://reqres.in/api/products";
 export default async function queryData(page: number = 1) {
   if (page === 1) {
     const response = await fetch(`${BASIC_API}?page=1`);
+    if (response.ok === false) {
+      throw new Error(response.status.toString());
+    }
     const data = await response.json();
     return { page1: data, page2: { data: [] } };
   } else {
     const response1 = await fetch(`${BASIC_API}?page=${page - 1}`);
     const response2 = await fetch(`${BASIC_API}?page=${page}`);
+    if (response1.ok === false || response2.ok === false) {
+      const invalidResponseStatus = response1.ok === false ? response1.status : response2.status;
+      throw new Error(invalidResponseStatus.toString());
+    }
     const data1 = response1.json();
     const data2 = response2.json();
     const allData = await Promise.all([data1, data2]);
--- src/queryFunctions/querySearchData.ts
+++ src/queryFunctions/querySearchData.ts
@@ -3,6 +3,9 @@ const BASIC_API = "https://reqres.in/api/products";
 export default async function queryData(searchQuery: number) {
   if (!searchQuery) return {};
   const response = await fetch(`${BASIC_API}?id=${searchQuery}`);
+  if (response.ok === false) {
+    throw new Error(response.status.toString());
+  }
   const data = await response.json();
   return data;
 }
